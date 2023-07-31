28b57fa0658f33c28571e4bff816634fc171d66c
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-07-31 20:47:36 -0300

    add post method

--- api-products/src/main/java/com/larissa/apiproducts/dtos/ProductRecordDto.java
+++ api-products/src/main/java/com/larissa/apiproducts/dtos/ProductRecordDto.java
@@ -1,6 +1,9 @@
 package com.larissa.apiproducts.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record ProductRecordDto(String name, BigDecimal value) {
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
 }
