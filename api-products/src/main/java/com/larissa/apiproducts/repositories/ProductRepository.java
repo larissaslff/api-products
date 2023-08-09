a2f7c2990304567a833500ecbd2af44db5187a37
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-08-09 20:21:04 -0300

    Adding pagination

--- api-products/src/main/java/com/larissa/apiproducts/repositories/ProductRepository.java
+++ api-products/src/main/java/com/larissa/apiproducts/repositories/ProductRepository.java
@@ -2,10 +2,11 @@ package com.larissa.apiproducts.repositories;
 
 import com.larissa.apiproducts.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+public interface ProductRepository extends PagingAndSortingRepository<ProductModel, UUID> {
     List<ProductModel> findByNameContainingIgnoreCase(String name);
 }
--- api-products/src/test/java/com/larissa/apiproducts/controllers/ProductControllerTest.java
+++ api-products/src/test/java/com/larissa/apiproducts/controllers/ProductControllerTest.java
@@ -6,7 +6,6 @@ import com.larissa.apiproducts.models.ProductModel;
 import com.larissa.apiproducts.repositories.ProductRepository;
 import com.larissa.apiproducts.services.ProductService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
